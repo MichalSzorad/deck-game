0644ec7906f037127e687646815a96b8187007ba
Michal Szorád <miso.szorad@gmail.com>
2021-06-12 12:14:11 +0100

    Add icons and core styles

---
+++ src/theme.tsx
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    light: '#F2F2F7',
+    dark: '#152935',
+    orange: '#FB592D',
+    blue: '#266EF1',
+    green: '#007A87',
+  },
+
+  spacing: {
+    micro: '4px',
+    tiny: '8px',
+    small: '16px',
+    medium: '24px',
+    large: '32px',
+    xLarge: '48px',
+    giant: '64px',
+  },
+};
